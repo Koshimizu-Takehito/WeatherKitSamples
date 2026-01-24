8e1b306e3f6960a63d8ac6569e24f4bc207f2398
takehito <takehito.koshimizu@gmail.com>
2026-01-24 17:45:19 +0900

    chore: initialize Xcode project

---
+++ WeatherKitSamples/WeatherKitSamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherKitSamplesApp.swift
+//  WeatherKitSamples
+//
+//  Created by Takehito Koshimizu on 2026/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherKitSamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
